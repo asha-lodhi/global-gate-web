6fb1756b4a13b3957d7cb7c53ba97f0e2d6be663
Asha <ashalodhi1097@gmail.com>
2024-05-13 18:40:31 +0530

    UI updates

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -14,7 +14,7 @@ const AboutSection = () => {
     <div class="w-screen flex h-screen relative">
       <img src={aboutBG} className='absolute h-auto w-screen object-fill z-[0]' />
       <img src={aboutBgTrans} className='absolute h-auto w-screen object-fill z-[1] ' />
-      <div className='flex flex-col justify-center items-center z-10 pt-14'>
+      <div className='flex flex-col justify-center items-center z-10 pt-20'>
         <div className='w-[90%] max-w-screen-xl'>
           <h3 className='text-[28px] text-[#FDCBCB] text-center'>Global Gate Services is a place where businesses come alive with the pulse of AI-driven insights, where every click sparks new possibilities, and where learning is as delightful as it is enlightening. That's the vision behind Globalgate – a vision fueled by passion, powered by technology, and dedicated to empowering businesses like yours to thrive in the global marketplace</h3>
         </div>
