84b817f84dbd80f5cd3f38293b10ad3cc7b34714
Asha <ashalodhi1097@gmail.com>
2024-05-13 12:26:48 +0530

    first commit

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../component/navbar";
+import HeroSection from "../component/heroSection";
+const dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default dashboard;
