0151f241cf4dcca90d9c8e215df89e436dfabb63
Asha <ashalodhi1097@gmail.com>
2024-05-13 13:37:45 +0530

    UI updates

--- src/component/heroSection.jsx
+++ src/component/heroSection.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const heroSection = () => {
+const HeroSection = () => {
   return (
-    <div>heroSection</div>
-  )
-}
+    <>
+      <div className=" flex w-full h-1/2  relative  justify-center items-center ">
+        <img
+          //   src={WebDevelopment}
+          alt="services"
+          className="w-full h-[800px] object-cover opacity-90"
+        />
+        <div className="flex absolute top-10 mt-48 flex-col justify-center items-center px-10">
+          <span className=" text-2xl md:text-6xl font-sans font-seminbold text-black">
+            Global B2B food & drink Marketplace
+          </span>
+          <p className="text-sm md:text-base font-light text-white  text-center line-clamp-2 md:w-[500px] my-8">
+            Connecting food and beverage companies worldwide with AI-powered
+            insights, efficient operations, and personalized experiences.
+          </p>
+          <button
+            className=" text-[20px]  bg-white text-[#146AA1] font-poppins px-4 py-2 rounded-full"
+            // onClick={() => scrollToTarget("Contact-Us")}
+          >
+            Explore
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default heroSection
+export default HeroSection;
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import React from "react";
+import MainLogo from "../assets/main-logo.png";
+import user from "../assets/user-image.jpeg";
 
 const navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <>
+      <nav class="bg-[#24323f]">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 bg-[#24323f]">
+          <span className="flex items-center space-x-3">
+            <img src={MainLogo} className="h-32 w-36" alt="Logo" />
+          </span>
+          <div class="flex items-center align-middle md:order-2 space-x-3 md:space-x-0">
+            <div
+              class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+              id="navbar-user"
+            >
+              <ul class=" flex align-middle  justify-center items-center flex-col text-xl p-4 md:p-0  border  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-[#24323f]">
+                <li>
+                  <span
+                    className="block py px-3 text-white hover:underline hover:text-blue-700 rounded md:bg-transparent md:p-0 md:dark:text-white"
+                    aria-current="page"
+                  >
+                    Home
+                  </span>
+                </li>
+                <li>
+                  <span className="block py-2 px-3 text-white hover:underline hover:text-blue-700 rounded md:bg-transparent md:p-0 md:dark:text-white">
+                    About
+                  </span>
+                </li>
+                <li>
+                  <span className="block py-2 px-3 text-white hover:underline hover:text-blue-700 rounded md:bg-transparent md:p-0 md:dark:text-white">
+                    Products
+                  </span>
+                </li>
+                <li>
+                  <span className="block py-2 px-3 text-white hover:underline hover:text-blue-700 rounded md:bg-transparent md:p-0 md:dark:text-white">
+                    Services
+                  </span>
+                </li>
+                <li>
+                  <span className="block py-2 px-3 text-white hover:underline hover:text-blue-700 rounded md:bg-transparent md:p-0 md:dark:text-white">
+                    Blogs
+                  </span>
+                </li>
+                <div
+                  class="z-50 hidden my-4 text-base list-none bg-[#24323f] divide-y divide-gray-100 rounded-lg shadow dark:bg-[#24323f] dark:divide-gray-600"
+                  id="user-dropdown"
+                >
+                  <div class="px-4 py-3">
+                    <span class="block text-sm text-gray-900 dark:text-white">
+                      Bonnie Green
+                    </span>
+                    <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                      name@flowbite.com
+                    </span>
+                  </div>
+                  <ul class="py-2 " aria-labelledby="user-menu-button">
+                    <li>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#24323f] dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Dashboard
+                      </span>
+                    </li>
+                    <li>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-[#24323f] dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Settings
+                      </span>
+                    </li>
+                    <li>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Earnings
+                      </span>
+                    </li>
+                    <li>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                        Sign out
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+                <button
+                  type="button"
+                  class="flex text-sm bg-[#24323f] rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                  id="user-menu-button"
+                  aria-expanded="false"
+                  data-dropdown-toggle="user-dropdown"
+                  data-dropdown-placement="bottom"
+                >
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    className="w-14 h-14 rounded-full object-cover"
+                    src={user}
+                    alt="user photo"
+                  />
+                </button>
+              </ul>
+            </div>
 
-export default navbar
+            <button
+              data-collapse-toggle="navbar-user"
+              type="button"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-user"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default navbar;
