84f770d2d15fd56d96b5459ea03fd6af59cc39b1
Sandeep <c.sandeepkumar@mulberri.io>
2024-05-13 16:26:57 +0530

    About section

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../component/navbar";
 import HeroSection from "../component/heroSection";
+import AboutSection from "../component/about";
 const dashboard = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </>
   );
 };
