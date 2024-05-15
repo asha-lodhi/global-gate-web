7b25d305e77ebfd3629594aa4c1776bb56346a19
Asha <ashalodhi1097@gmail.com>
2024-05-15 21:08:12 +0530

    chat modal added

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import Products from "../component/products";
 import AboutSection from "../component/about";
 import HomeSection from "../component/heroSection";
+import ChatBox from "../component/chatBox";
 
-const dashboard = () => {
+const Dashboard = () => {
   return (
     <>
       <HomeSection />
       <AboutSection />
       <Products />
+      <ChatBox />
     </>
   );
 };
 
-export default dashboard;
+export default Dashboard;
