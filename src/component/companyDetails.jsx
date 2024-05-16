34b4fed2abe87280b8874517a9d7fa245652e7a4
Asha <ashalodhi1097@gmail.com>
2024-05-16 18:30:21 +0530

    added analytic button

--- src/component/companyDetails.jsx
+++ src/component/companyDetails.jsx
@@ -25,27 +25,14 @@ const CompanyDetails = (props) => {
 
           <div class="w-full md:w-9/12 mx-2 h-64  flex flex-col justify-center items-center">
             <div class="bg-white p-3 shadow-sm rounded-sm">
-              <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 py-2">
-                {/* <span clas="text-green-500">
-                  <svg
-                    class="h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    />
-                  </svg>
-                </span> */}
-                <span class="tracking-wide text-xl">
+              <div class="flex items-center  justify-between space-x-2 font-semibold text-gray-900 leading-8 py-2">
+                <span class="tracking-wide text-xl font-semibold">
                   {" "}
                   All About the company
                 </span>
+                <button className="bg-blue-400 rounded-xl px-4 py-1">
+                  Company Analytics
+                </button>
               </div>
               <div class="text-gray-700">
                 <div class="grid md:grid-cols-2 text-sm">
